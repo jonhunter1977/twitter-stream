7b57ea09e2405498af523aef6105ffed6b1307ac
Jon Hunter <jonhunter1977@gmail.com>
2017-07-14 10:25:19 +0100

    exit if not authenticated

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const socketServer = require('./lib/socketServer');
-
 const getStatusesSample = require('./lib/twitter').getStatusesSample;
-
 const statusSampleStream = getStatusesSample();
+
 statusSampleStream
   .on('response', (response) => {
     console.log('Response received : ' + response.statusCode);
+    if (response.statusCode === 401) {
+      console.log('Authentication failed');
+      process.exit();
+    }
   })
   .on('data', (data) => {
-    const twitterData  = data.toString('UTF8');
+    const twitterData = data.toString('UTF8');
     try {
       const jsonData = JSON.parse(twitterData);
       if (jsonData.text) {
@@ -20,4 +23,7 @@ statusSampleStream
     } catch (e) {
       // console.error('Unable to parse data');
     }
+  })
+  .on('error', (err) => {
+    console.log('Error on stream : ', err);
   });
